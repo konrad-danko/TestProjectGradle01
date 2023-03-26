791c8520c4e357eeff07aa99b0aac4c88f7772fd
konrad-danko <konrad.danko@gmail.com>
2023-03-26 20:38:24 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/TestProjectGradle01/TestProjectGradle01Application.java
@@ -0,0 +1,13 @@
+package pl.coderslab.TestProjectGradle01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectGradle01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectGradle01Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/TestProjectGradle01/TestProjectGradle01ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.TestProjectGradle01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectGradle01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
